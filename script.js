d792d06ecaa3811fc5673244a5b4451bfc254391
domis23 <96925584+domis23@users.noreply.github.com>
2023-09-06 16:53:38 +0200

    v1.5 SuperUpdate

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const wheelExtraExtra = document.querySelector('#wheelExtra')
 const buttonExtra = document.querySelector('#button')
+const buttonDiv = document.querySelector('#btn')
+const divs = document.querySelectorAll('div')
+const paragraphs = document.querySelectorAll('p')
 
 const RotStart = () => {
-  wheelExtraExtra.classList.add('wheelRot')
-  setTimeout(alercik, 2100)
+  hide()
+  setTimeout(reset, 8001)
+  setTimeout(alercik, 8000)
+  setTimeout(wheelrot, 5100)
+  setTimeout(biggerBetter, 5000)
+  // setTimeout(buttonText, 800)
+  bigger()
 }
-
+const reset = () => {
+  location.reload()
+}
+const biggerBetter = () => {
+  wheelExtraExtra.classList.remove('bigger')
+  wheelExtraExtra.classList.add('biggerBetter')
+}
+const buttonText = () => {
+  buttonExtra.innerHTML = ''
+}
+const wheelrot = () => {
+  wheelExtraExtra.classList.add('wheelRot')
+}
 const alercik = () => {
-  alert('Niestety 🤷‍♂️, tak bywa.')
+  alert("You're gay! 🥳")
+}
+const hide = () => {
+  buttonExtra.classList.remove('button')
+  buttonExtra.classList.add('buttonHide')
+  // buttonExtra.classList.remove('button')
+  // buttonExtra.classList.add('hideBtn')
+  paragraphs[0].classList.remove('green', 'red')
+  paragraphs[0].classList.add('hideText')
+  paragraphs[1].classList.remove('green', 'red')
+  paragraphs[1].classList.add('hideText')
+  divs[1].classList.remove('literki')
+  divs[1].classList.add('hideText')
+}
+const bigger = () => {
+  wheelExtraExtra.classList.add('bigger')
 }
 
 buttonExtra.addEventListener('click', RotStart)
